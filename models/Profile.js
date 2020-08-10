e4c1071ac87b4171787b6a23d184f594d33c7479
Kevin <knphillips01@gmail.com>
2020-08-10 15:49:54 -0500

    added some fields for profile schema

---
+++ models/Profile.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  status: {
+    type: String
+  },
+  rvtype: {
+    type: String
+  },
+  wherefrom: {
+    type: String
+  },
+  agerange: {
+    type: String
+  },
+  bio: {
+    type: String
+  },
+  kids: {
+    type: String
+  },
+  pets: {
+    type: String
+  },
+  favplaces: {
+    type [String]
+  },
+  advice: {
+    type: [String]
+  },
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    instagram: {
+      type: String
+    },
+    website: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
