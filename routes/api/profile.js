207b2b52cb67723ae6f61c91cc56000b48ead9c6
Kevin <knphillips01@gmail.com>
2020-08-12 15:41:39 -0500

    added route to delete favorite places

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -212,4 +212,27 @@ router.put(
   }
 );
 
+// @route    DELETE api/profile/favplaces/:fav_id
+// @desc     Delete favorite place from profile
+// @access   Private
+router.delete('/favplaces/:fav_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.favplaces
+      .map(item => item.id)
+      .indexOf(req.params.fav_id);
+
+    profile.favplaces.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
