c283e8889611849e164f64f3c9a888b3bd956577
Kevin <knphillips01@gmail.com>
2020-08-14 13:32:37 -0500

    added protected route for dashboard

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const Dashboard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+Dashboard.propTypes = {
+
+}
+
+export default Dashboard;
