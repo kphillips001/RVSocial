0118c6ab07d26201633ac525b75d5ec5f00a55d2
Kevin <knphillips01@gmail.com>
2020-08-15 14:04:34 -0500

    Create Profile form done

---
+++ client/src/components/dashboard/DashboardActions.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const DashboardActions = () => {
+  return (
+    <div className='dash-buttons'>
+      <Link to='/edit-profile' className='btn btn-light'>
+        <i className='fas fa-user-circle text-primary' /> Edit Profile
+      </Link>
+      <Link to='/add-experience' className='btn btn-light'>
+        <i className='fas fa-campground text-primary' /> Add Favorite Places to Camp
+      </Link>
+      <Link to='/add-education' className='btn btn-light'>
+        <i className='far fa-comment text-primary' /> Add Camping Advice
+      </Link>
+    </div>
+  );
+};
+
+export default DashboardActions;
---
+++ client/src/components/profile-forms/CreateProfile.js
@@ -0,0 +1,210 @@
+mport React, { useEffect, useState, Fragment } from "react";
+import { Link, withRouter, Redirect } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { createProfile, getCurrentProfile } from "../../actions/profile";
+
+const Createprofile = ({
+  profile: { profile, loading },
+  createProfile,
+  getCurrentProfile,
+  history
+}) => {
+  const [formData, setFormData] = useState({
+    status: "",
+    rvtype: "",
+    wherefrom: "",
+    agerange: "",
+    status: "",
+    bio: "",
+    twitter: "",
+    facebook: "",
+    youtube: "",
+    instagram: "",
+    website: ""
+  });
+  const [displaySocialInputs, toggleSocialInputs] = useState(false);
+  const {
+    status,
+    rvtype,
+    wherefrom,
+    agerange,
+    status,
+    bio,
+    twitter,
+    facebook,
+    youtube,
+    instagram,
+    website
+  } = formData;
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const onSubmit = e => {
+    e.preventDefault();
+    createProfile(formData, history);
+  };
+  useEffect(() => {
+    getCurrentProfile();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [getCurrentProfile]);
+  return loading && profile === null ? (
+    <Redirect to='/dashboard' />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Create Your Profile</h1>
+      <p className='lead'>
+        <i className='fas fa-user' /> Let's get some information to make your
+        profile stand out
+      </p>
+      <small>* = required field</small>
+      <form className='form' onSubmit={e => onSubmit(e)}>
+      <div className='form-group'>
+          <select name='status' value={status} onChange={onChange}>
+            <option value='0'>* Select Rver Status</option>
+            <option value='Full-time Retired'>Full-time Retired</option>
+            <option value='Full-time Working'>Full-time Working</option>
+            <option value='Vacationer'>Vacationer</option>
+            <option value='Weekender'>Weekender</option>
+            <option value='Student or Learning'>Student or Learning</option>
+           </select>
+          <small className='form-text'>
+            Give us an idea of what type of RV enthusiast you are!
+          </small>
+        </div>
+        <div className='form-group'>
+          <select name='rvtype' value={rvtype} onChange={onChange}>
+            <option value='0'>* Select Rver Type</option>
+            <option value='Class A Motorhome'>Class A Motorhome</option>
+            <option value='Class C Motorhome'>Class C Motorhome</option>
+            <option value='Class B Camper Van'>Class B Camper Van</option>
+            <option value='Fifth Wheeel Camper'>Fifth Wheeel Camper</option>
+            <option value='Toy Hauler'>Toy Hauler</option>
+            <option value='Travel Trailer'>Toy Hauler</option>
+            <option value='Teardrop Camper'>Teardrop Camper</option>
+            <option value='Truck Camper'>Class B Camper Van</option>
+            <option value='Pop-up Camper'>Class B Camper Van</option>
+           </select>
+          <small className='form-text'>
+            What kind of camper do you own?
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Where are you from?'
+            name='wherefrom'
+            value={wherefrom}
+            onChange={onChange}
+          />
+          <small className='form-text'>
+            Where are you from?
+          </small>
+        </div>
+        <div className='form-group'>
+          <select name='agerange' value={agerange} onChange={onChange}>
+            <option value='0'>* Select Your Age Range</option>
+            <option value='Twenties'>Twenties</option>
+            <option value='Thirties'>Thirties</option>
+            <option value='Fourties'>Fourties</option>
+            <option value='Fifty +'>Fifty +</option>
+           </select>
+          <small className='form-text'>
+            What is your age range?
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='A brief bio of yourself'
+            name='bio'
+            value={bio}
+            onChange={onChange}
+          />
+          <small className='form-text'>
+            Why do you like the camping lifestyle?
+          </small>
+        </div>
+        <div className='my-2'>
+          <button
+            onClick={() => toggleSocialInputs(!displaySocialInputs)}
+            type='button'
+            className='btn btn-light'
+          >
+            Add Social Network Links
+          </button>
+          <span>Optional</span>
+        </div>
+        {displaySocialInputs && (
+          <Fragment>
+            <div className='form-group social-input'>
+              <i className='fab fa-facebook fa-2x' />
+              <input
+                type='text'
+                placeholder='Facebook URL'
+                name='facebook'
+                value={facebook}
+                onChange={onChange}
+              />
+            </div>
+            <div className='form-group social-input'>
+              <i className='fab fa-twitter fa-2x' />
+              <input
+                type='text'
+                placeholder='Twitter URL'
+                name='twitter'
+                value={twitter}
+                onChange={onChange}
+              />
+            </div>
+            <div className='form-group social-input'>
+              <i className='fab fa-youtube fa-2x' />
+              <input
+                type='text'
+                placeholder='YouTube URL'
+                name='youtube'
+                value={youtube}
+                onChange={onChange}
+              />
+            </div>
+            <div className='form-group social-input'>
+              <i className='fab fa-instagram fa-2x' />
+              <input
+                type='text'
+                placeholder='Instagram URL'
+                name='instagram'
+                value={instagram}
+                onChange={onChange}
+              />
+            </div>
+            <div className='form-group social-input'>
+              <i className='fas fa-globe fa-2x' />
+              <input
+                type='text'
+                placeholder='Website URL'
+                name='website'
+                value={website}
+                onChange={onChange}
+              />
+            </div>
+          </Fragment>
+        )}
+        <input type='submit' className='btn btn-primary my-1' />
+        <Link className='btn btn-light my-1' to='/dashboard'>
+          Go Back
+        </Link>
+      </form>
+    </Fragment>
+  );
+};
+Createprofile.propTypes = {
+  createProfile: PropTypes.func.isRequired,
+  getCurrentProfile: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  profile: state.profile,
+});
+export default connect(
+  mapStateToProps,
+  { createProfile, getCurrentProfile },
+)(withRouter(Createprofile));
