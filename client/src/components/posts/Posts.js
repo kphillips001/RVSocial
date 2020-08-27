cc93d8b9e58c7d7e5141917e148f830473ab7b71
Kevin <knphillips01@gmail.com>
2020-08-27 14:32:50 -0500

    set up Post Item Component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,7 +1,8 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
 import { getPosts } from '../../actions/post';
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
@@ -9,12 +10,23 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
     getPosts()
   }, [getPosts]);
 
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return loading ? (
+    <Spinner />
+  ) : (
+    <>
+      <h1 className='large text-primary'>Posts</h1>
+      <p className='lead'>
+        <i className='fas fa-user' /> Welcome to the RVing Community
+      </p>
+      {/* <PostForm /> */}
+      <div className='posts'>
+        {posts.map(post => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </>
+  );
+};
 
 Posts.propTypes = {
   getPosts: PropTypes.func.isRequired,
